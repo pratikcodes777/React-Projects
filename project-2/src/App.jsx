90447d23e74ff8cdad362661a3c6f3ffcf5657c5
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-23 18:33:07 +0545

    Contact us modules

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navigation from './components/Navigation/Navigation.jsx'
+import ContactHeader from './components/ContactHeader/ContactHeader.jsx'
 
 function App() {
   return (
     <div>
        <Navigation/>
+       <ContactHeader></ContactHeader>
     </div>
      
   )
