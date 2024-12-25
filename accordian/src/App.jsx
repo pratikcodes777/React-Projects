b02700220b271b81a2404d0d5917de1f0aaddac4
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-25 21:40:54 +0545

    hex color generation

--- accordian/src/App.jsx
+++ accordian/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       {/* <Accordian></Accordian> */}
 
       {/* Random Color Component */}
-      <RandomColor></RandomColor>
+      <RandomColor/>
     </div>
   )
 }
--- accordian/src/components/random-color/RandomColor.jsx
+++ accordian/src/components/random-color/RandomColor.jsx
@@ -5,6 +5,28 @@ const RandomColor = () => {
     const [typeOfColor, setTypeOfColor] = useState("hex")
     const [color, setColor] = useState("#000000")
 
+    const randomColorUtility = (length) =>{
+        return Math.floor(Math.random()* length)
+    }
+
+    const createHexColor = () => {
+        const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+        let hexColor = "#"
+
+        for(let i=0; i<6 ; i++){
+            hexColor += hex[randomColorUtility(hex.length)]
+        }
+
+        console.log(hexColor)
+        setColor(hexColor)
+
+    }
+
+    const createRgbColor = () => {
+        
+    }
+
+
 
     return ( 
         <div style={{
@@ -12,9 +34,9 @@ const RandomColor = () => {
             width:'100vw',
             background: color
         }}>
-            <button>Generate HEX code</button>
-            <button>Generate RGB code</button>
-            <button>Generate random color</button>
+            <button onClick={() =>setTypeOfColor('hex')}>Generate HEX code</button>
+            <button onClick={() =>setTypeOfColor('rgb')}>Generate RGB code</button>
+            <button onClick={typeOfColor === 'hex' ? createHexColor : createRgbColor}>Generate random color</button>
         </div>
      );
 }
