7fb12a5912b6d570e5061c851de146ec3c6665f9
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-25 18:15:36 +0545

    multiple selection done

--- accordian/src/components/accordian/Accordian.jsx
+++ accordian/src/components/accordian/Accordian.jsx
@@ -1,43 +1,64 @@
 import { useState } from "react";
 import data from "./data";
-import './style.css';
-
+import "./style.css";
 
 const Accordian = () => {
-    const [selected, setSelected] = useState(null)
+  const [selected, setSelected] = useState(null);
+  const [multiSelection, setMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-    const handleSingleClick =(getId) =>{
-        console.log(getId)
-        setSelected(getId === selected ? null : getId)
-    }
+  const handleSingleClick = (getId) => {
+    console.log(getId);
+    setSelected(getId === selected ? null : getId);
+  };
 
-    return ( 
-            <div className="wrapper">
-                <div className="accordian">
-                    {data && data.length > 0 ? (
-                        data.map((item) => (
-                            // Each item in the accordian
-                            <div key={item.id} className="item">
-                                <div onClick={()=>handleSingleClick(item.id)} className="title">
-                                    {/* Display the question */}
-                                    <h3>{item.question}</h3>
-                                    {/* Expand/Collapse indicator */}
-                                    <span>+</span>
-                                </div>
-                                {
-                                    selected === item.id ?
-                                    <div className="content">{item.answer}</div>
-                                    : null
-                                }
-                            </div>
-                        ))
-                    ) : (
-                        // Fallback when no data is available
-                        <div>No data found</div>
-                    )}
-                </div>
+  const handleMultiSelection= (getId) =>{
+    let cpyMultiple = [...multiple]
+    const currentIdIndex = cpyMultiple.indexOf(getId)
+    if(currentIdIndex === -1) cpyMultiple.push(getId)
+        else cpyMultiple.splice(currentIdIndex, 1)
+
+    setMultiple(cpyMultiple)
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setMultiSelection(!multiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((item) => (
+            // Each item in the accordian
+            <div key={item.id} className="item">
+              <div
+                onClick={
+                  multiSelection
+                    ? () => handleMultiSelection(item.id)
+                    : () => handleSingleClick(item.id)
+                }
+                className="title"
+              >
+                {/* Display the question */}
+                <h3>{item.question}</h3>
+                {/* Expand/Collapse indicator */}
+                <span>+</span>
+              </div>
+              {
+                multiSelection ? 
+                multiple.indexOf(item.id) !== -1 &&
+                <div className="content">{item.answer}</div> :
+                selected === item.id && <div className="content">{item.answer}</div>
+              }
             </div>
-     );
-}
- 
+          ))
+        ) : (
+          // Fallback when no data is available
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default Accordian;
