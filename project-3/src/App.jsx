2623da74664f9838314d1fda7c689ca9c6f02f27
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-26 20:41:25 +0545

    game mode toggle

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components'
 import StartGame from './components/StartGame'
+import GamePlay from './components/GamePlay'
+import { useState } from 'react'
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(true)
+
+  const toggleGameMode = () =>{
+    setIsGameStarted((prev) => !prev)
+  }
 
   return (
     <div className="App">
-      <StartGame></StartGame>
+      {isGameStarted?<GamePlay></GamePlay>:<StartGame toggle={toggleGameMode}></StartGame>}
     </div>
   )
 }
