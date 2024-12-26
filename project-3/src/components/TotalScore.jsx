c2367a7be0b4d8e866941b494df237d752d9364d
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-26 20:42:01 +0545

    total score component styling

---
+++ project-3/src/components/TotalScore.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const TotalScore = () => {
+    return ( 
+            <ScoreContainer>
+                <h1>0</h1>
+                <p>Total Score</p>
+            </ScoreContainer>
+     );
+}
+ 
+export default TotalScore;
+
+const ScoreContainer = styled.div`
+    max-width: 220px;
+    text-align: center;
+    h1{
+        color: #000;
+        font-family: "Poppins", sans-serif;
+        font-size: 100px;
+        font-weight: 600;
+        line-height: 100px;
+    }
+
+    p{
+        color: #000;
+        font-family: Poppins;
+        font-size: 18px;
+        font-weight: 500;
+    }
+
+
+    
+
+`
