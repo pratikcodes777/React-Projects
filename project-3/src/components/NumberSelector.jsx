31d138e34a11a202f4ed1cc908882a19407ae2b1
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-27 12:47:14 +0545

    Passing props on styled components

--- project-3/src/components/NumberSelector.jsx
+++ project-3/src/components/NumberSelector.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 
 const NumberSelector = () => {
+
+    const [selectedNum, setSelectedNum]= useState()
+
     const arrNum = [1,2,3,4,5,6]
     return ( 
         <div>
 
         {arrNum.map((val,i) =>(
-            <Box key={i}>{val}</Box>
+            <Box
+            isselected = {val === selectedNum}
+            onClick={()=>setSelectedNum(val)}
+            key={i}>{val}</Box>
         )
         )}
+        
 
         </div>
      );
@@ -29,5 +37,8 @@ const Box = styled.div`
     font-family: "Poppins";
     font-size: 24px;
     font-weight: 700;
+
+    background-color: ${(props)=> (props.isselected? "black" : "white")};
+    color: ${(props)=> (props.isselected? "#FFF" : "#000")};
 
 `
