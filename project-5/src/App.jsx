290a63a93c6173a157a3f0ecf3bf4535c8910527
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-30 22:35:24 +0545

    project and tailwind css setup

---
+++ project-5/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return ( 
+    <>
+    <div className="bg-red-400 flex justify-between">App</div>
+
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+   );
+}
+ 
+export default App;
