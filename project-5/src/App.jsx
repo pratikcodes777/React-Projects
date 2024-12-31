6615bac6a35bce978cbb20de68418016a606e359
Pratik Barakoti <pratik.barakoti01@gmail.com>
2024-12-31 16:03:55 +0545

    tailwind css testing with prettier extension

--- project-5/src/App.jsx
+++ project-5/src/App.jsx
@@ -1,13 +1,12 @@
 const App = () => {
-  return ( 
+  return (
     <>
-    <div className="bg-red-400 flex justify-between">App</div>
+      <div className="flex justify-between bg-red-400">App</div>
+      <div className="flex justify-start bg-slate-500">New App</div>
 
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
-   );
-}
- 
+  );
+};
+
 export default App;
