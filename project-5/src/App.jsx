aa7e0ef2f05ad29af0e2917647b5657dd6513a89
Pratik Barakoti <pratik.barakoti01@gmail.com>
2025-01-02 18:47:53 +0545

    Search functionality and plus icon added

--- project-5/src/App.jsx
+++ project-5/src/App.jsx
@@ -1,9 +1,23 @@
 import Navbar from "./components/Navbar";
+import { FaSearch } from "react-icons/fa";
+import { FaPlusCircle } from "react-icons/fa";
 
 const App = () => {
   return (
-    <div className="max-w-[370px] mx-auto">
-      <Navbar ></Navbar>
+    <div className="mx-auto max-w-[370px] px-4">
+      <Navbar></Navbar>
+
+      <div className="flex gap-2">
+        <div className="relative flex items-center flex-grow">
+          <FaSearch className="absolute ml-1 text-2xl text-white"></FaSearch>
+          <input
+            type="text"
+            placeholder="Search Contact"
+            className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+          />
+        </div>
+          <FaPlusCircle className="text-white text-[40px] cursor-pointer" />
+      </div>
     </div>
   );
 };
--- project-5/src/components/Navbar.jsx
+++ project-5/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="m-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
+    <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
       <img src="public/logos_firebase.svg" alt="logo image" />
       <h1>Firebase Contact App</h1>
     </div>
