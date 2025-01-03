cac6e82eaea4ce1d7ec64ee24b5991ac2e13c75b
Pratik Barakoti <pratik.barakoti01@gmail.com>
2025-01-03 15:12:36 +0545

    Modal creation using Portal

--- project-5/src/components/Modals.jsx
+++ project-5/src/components/Modals.jsx
@@ -1,10 +1,28 @@
-const Modals = ({isOpen , onClose, children}) => {
-    return (  
-        <>{
-            isOpen && <div>Models</div>
-        }
+import { IoMdClose } from "react-icons/io";
+import {createPortal} from "react-dom"
+
+const Modals = ({ isOpen, onClose, children }) => {
+  return createPortal(
+    <>
+      {isOpen && (
+        <>
+          <div className="relative z-20 m-auto min-h-[200px] max-w-[80%] bg-white p-4">
+            <div className="flex justify-end">
+              <IoMdClose
+                onClick={onClose}
+                className="cursor-pointer text-3xl"
+              />
+            </div>
+          </div>
+          <div
+            onClick={onClose}
+            className="z-100 absolute top-0 h-screen w-screen backdrop-blur"
+          ></div>
         </>
-    );
-}
- 
+      )}
+    </>,
+    document.getElementById("modal-root")
+  );
+};
+
 export default Modals;
