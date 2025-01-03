f75a6ffd1839342c801b00692a54a21738616a83
Pratik Barakoti <pratik.barakoti01@gmail.com>
2025-01-03 15:51:01 +0545

    Modals data submission

--- project-5/src/components/AddAndUpdate.jsx
+++ project-5/src/components/AddAndUpdate.jsx
@@ -1,6 +1,19 @@
+import { addDoc, collection } from "firebase/firestore";
 import Modals from "./Modals";
 import { Formik, Form, Field } from "formik";
+import { db } from "../config/firebase";
+
 const AddAndUpdate = ({ isOpen, onClose }) => {
+
+    const addContact = async(contact) =>{
+        try {
+            const contactRef = collection(db , "contacts")
+            await addDoc(contactRef , contact)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
   return (
     <div>
       <Modals isOpen={isOpen} onClose={onClose}>
@@ -11,17 +24,18 @@ const AddAndUpdate = ({ isOpen, onClose }) => {
         }}
         onSubmit={(values) => {
             console.log(values)
+            addContact(values)
         }}
         >
           <Form className="flex flex-col gap-2 ">
             <div className="flex flex-col gap-1">
               <label htmlFor="name">Name</label>
-              <Field className="h-9 border-2" name="name"></Field>
+              <Field className="h-9 border-2 pl-1" name="name"></Field>
             </div>
 
             <div className="flex flex-col gap-1">
               <label htmlFor="email">Email</label>
-              <Field className="h-9 border-2" name="email"></Field>
+              <Field className="h-9 border-2 pl-1" name="email"></Field>
             </div>
             <button className="border-2 rounded-md bg-orange px-5 py-1 self-end">Add Contact</button>
           </Form>
